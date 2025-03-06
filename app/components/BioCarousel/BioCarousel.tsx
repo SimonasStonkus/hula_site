551dd386e1bfb54ef826da47a3f458102d29b1e7
SimonasStonkus <Simonas.stonkus@gmail.com>
2025-03-06 17:31:34 +0000

    carousel indicators

--- app/components/BioCarousel/BioCarousel.tsx
+++ app/components/BioCarousel/BioCarousel.tsx
@@ -2,20 +2,40 @@ import useEmblaCarousel from "embla-carousel-react";
 import BioCard from "../BioCard/BioCard";
 import "./BioCarousel.css";
 import { characters } from "../constants/Characters";
+import { DotButton, useDotButton } from "./CarouselButtons";
 export function BioCarousel({ menuOpen }: { menuOpen: boolean }) {
-  const [emblaRef] = useEmblaCarousel({ loop: true, dragFree: false });
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    dragFree: false,
+  });
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        {characters.map((character) => {
-          return (
-            <div className="embla__slide" key={character.name}>
-              <BioCard menuOpen={menuOpen} character={character} />
-            </div>
-          );
-        })}
+    <>
+      <div className="embla" ref={emblaRef}>
+        <div className="embla__container">
+          {characters.map((character) => {
+            return (
+              <div className="embla__slide" key={character.name}>
+                <BioCard menuOpen={menuOpen} character={character} />
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
+
+      <div className="embla__dots">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            onClick={() => onDotButtonClick(index)}
+            className={"embla__dot".concat(
+              index === selectedIndex ? " embla__dot--selected" : ""
+            )}
+          />
+        ))}
+      </div>
+    </>
   );
 }
